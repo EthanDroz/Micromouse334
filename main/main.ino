b0e7faf53aee78b66ecdabe8a1fbb7d45382afe8
Ethan_Droz <Ethan_Droz@outlook.com>
2025-03-29 23:26:20 -0400

    Added sonar rangefinder function in mm-utils, set pinmodes in main.

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,30 @@
 
+#include "mm-utils.h";
 
+unsigned long current_time;
+unsigned long prev_time;
+unsigned long trigger1;
+int trigPin = 1;
+int echoPin = 2;
 
+float range;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  pinMode(1, INPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  current_time = millis();
 
+  if (current_time - prev_time > trigger1)
+  {
+    range = sonar_rangefinder(echoPin, trigPin);
+  }
 }
--- main/mm-utils.h
+++ main/mm-utils.h
@@ -1,5 +1,15 @@
 
 
-void check_sensors() {
-    
+// Fn to find distance for sonar sensor.
+
+float sonar_rangefinder(echoPin, trigPin)
+{
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    float duration = pulseIn(echoPin, HIGH, 11500);
+    return float distance_cm = (34300.0 * duration * 1.0e-6) * 0.5;
 }
