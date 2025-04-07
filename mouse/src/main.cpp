fc29fac3c94b764e5c471342e6694a24b1cd0acf
Ethan_Droz <Ethan_Droz@outlook.com>
2025-04-07 11:13:13 -0400

    Initialization of IR distance finder

--- mouse/src/main.cpp
+++ mouse/src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// timer declarations;
+unsigned long current_time;
+unsigned long prev_time_main;
+unsigned long time1 = 1000;
+
+// pin declarations;
+int irPin = 1;
+
+// function declarations:
+int ir_analog(int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(irPin, INPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  current_time = millis();
+
+  if (current_time - prev_time_main > time1) {
+    prev_time_main = current_time;
+
+  }
+
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int ir_analog(int Pin) {
+  int reading = analogRead(Pin);
+
+
 }
