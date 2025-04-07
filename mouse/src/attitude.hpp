512f423798a6caeb714ef311addf6e2cde27834c
Ethan_Droz <Ethan_Droz@outlook.com>
2025-04-07 15:33:46 -0400

    Added position functions

--- mouse/src/attitude.hpp
+++ mouse/src/attitude.hpp
@@ -1,13 +1,38 @@
 #ifndef attitude.hpp
 #define attitude.hpp
 
+// Includes
 #include <Arduino.h>
 #include "Wire.h"
 
+void current_position(float *position, float *accel, short dt) {
 
-float current_accel() {
+    while (*position)
+    {
+        *position = *accel * dt;
+        *position++;
+        *accel++;
+    }
     
 }
 
+void attitude_serial_output(float *pos, float *acc, long current_time) {
+    Serial.print("Time: ");
+    Serial.print(current_time);
+    
+    Serial.print(", Position: ");
+    while (*pos) {
+        Serial.print(*pos);
+        Serial.print(", ");
+        *pos++;
+    }
+    Serial.print(", Acceleration: ");
+    while (*acc) {
+        Serial.print(*acc);
+        Serial.print(", ");
+        *acc++;
+    }
+    Serial.println(" ");
+}
 
 #endif
--- mouse/src/main.cpp
+++ mouse/src/main.cpp
@@ -3,15 +3,22 @@
 #include "attitude.hpp"
 #include "Wire.h"
 
-
-
 // timer declarations;
 unsigned long current_time;
 unsigned long prev_time_main;
-unsigned long time1 = 1000;
+unsigned long prev_time_att;
+unsigned short time1 = 100;
+unsigned short delta_att = 20;
 
 // pin declarations;
-int irPin = 1;
+int irPin[5] = {1,2,3,4,5};
+int echoPin = 6;
+int trigPin = 7;
+
+// position init;
+float position[2] = {0, 0};
+float accel[2] = {0, 0};
+
 
 // function declarations:
 
@@ -19,7 +26,7 @@ void setup() {
   // put your setup code here, to run once:
   Wire.begin();
   Serial.begin(9600);
-  pinMode(irPin, INPUT);
+  pinMode(irPin[0], INPUT);
 
 }
 
@@ -27,9 +34,13 @@ void loop() {
   // put your main code here, to run repeatedly:
   current_time = millis();
 
+  if (current_time - prev_time_att > delta_att) {
+    current_position(position, accel, delta_att);
+  }
+
   if (current_time - prev_time_main > time1) {
     prev_time_main = current_time;
-    int ir_1 = ir_analog(irPin, time1);
+    int ir_1 = ir_analog(irPin[0], time1);
   }
 
 }
